450efc3a11611e24a7049bd0c29a8bf891e6a676
Athos Phelipe <athosphelipe005@gmail.com>
2020-09-22 01:02:51 -0300

    Inicio do Projeto
    Tela de Login
    Tela de Cadastro de Usuário

---
+++ app/src/main/java/br/com/unicid/aplicativodentista/ui/activity/AcitivityLoginUsuario.kt
@@ -0,0 +1,12 @@
+package br.com.unicid.aplicativodentista.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.unicid.aplicativodentista.R
+
+class AcitivityLoginUsuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_usuario)
+    }
+}
---
+++ app/src/main/java/br/com/unicid/aplicativodentista/ui/activity/NovoUsuarioAcitivity.kt
@@ -0,0 +1,12 @@
+package br.com.unicid.aplicativodentista.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.unicid.aplicativodentista.R
+
+class NovoUsuarioAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novo_usuario)
+    }
+}
