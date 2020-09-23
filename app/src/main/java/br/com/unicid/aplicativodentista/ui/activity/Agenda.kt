0af0bdf017388f2038b9161d6698c140d676bc7b
Athos Phelipe <athosphelipe005@gmail.com>
2020-09-22 23:27:13 -0300

    Refinamento das Activits

---
+++ app/src/main/java/br/com/unicid/aplicativodentista/ui/activity/Agenda.kt
@@ -0,0 +1,21 @@
+package br.com.unicid.aplicativodentista.ui.activity
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import br.com.unicid.aplicativodentista.R
+
+class Agenda : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agenda)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
