23c44f028ca873de8b94e34f750ccf98a4686d5c
Athos Phelipe <athosphelipe005@gmail.com>
2020-10-11 12:44:19 -0300

    Atualizações de Design

--- app/src/main/java/br/com/unicid/aplicativodentista/ui/activity/AgendaActivity.kt
+++ app/src/main/java/br/com/unicid/aplicativodentista/ui/activity/AgendaActivity.kt
@@ -11,11 +11,6 @@ class AgendaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agenda)
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+//        setSupportActionBar(findViewById(R.id.toolbar))
     }
 }
